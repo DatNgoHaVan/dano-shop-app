9666863aaeb39702568d7bc082be7c8a3117e678
datngo <ngohavandat93ndc@gmail.com>
2020-08-20 21:14:10 +0700

    Fix undefine state

--- screens/shop/ProductsOverviewScreen.tsx
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
 import { IProductState } from '../../redux-store/products/products-reducer';
+import { StoreRootState } from '../../redux-store/root/root-reducer';
 
 interface IOwnProps {
 
 }
 
 const ProductsOverviewScreen = () => {
-  const products = useSelector(state => state.products.availableProducts);
+  const products = useSelector((state: StoreRootState) => state.products.availableProducts);
 
   console.log(products);
   return (
